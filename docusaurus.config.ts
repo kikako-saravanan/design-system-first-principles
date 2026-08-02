4bd9cf05d318bf76a0ecfdd5f89409e30836f1a0
4723859 <4723859@lowes.com>
2026-08-03 00:38:39 +0530

    Add custom favicon icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Design System First Principles',
   tagline: 'Learning Design Systems based on first principles',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
